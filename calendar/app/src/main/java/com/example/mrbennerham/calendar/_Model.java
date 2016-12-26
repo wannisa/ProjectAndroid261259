b03adfa4999c1a2647db6b42c81b8de71310a3f0
wannisa <aoy.wannisa1994@gmail.com>
2016-12-27 00:02:58 +0700

    ProjectAndroid261259

---
+++ calendar/app/src/main/java/com/example/mrbennerham/calendar/_Model.java
@@ -0,0 +1,96 @@
+package com.example.mrbennerham.calendar;
+
+/**
+ * Created by MR.BENNERHAM on 11/14/2016.
+ */
+public class _Model {
+
+    String name = "";
+    String nameNoti = "";
+    String year= "";
+    String mount= "";
+    String day= "";
+    String time= "";
+    String date= "";
+    String dateend= "";
+    String timer= "";
+    String result="";
+
+    public String getname(){
+        return name;
+    }
+
+    public void setname(String name){
+        this.name = name;
+    }
+    public String getyear(){
+        return year;
+    }
+
+    public void setyear(String year){
+        this.year = year;
+    }
+    public String getmount(){
+        return mount;
+    }
+
+    public void setmount(String mount){
+        this.mount = mount;
+    }
+    public String getday(){
+        return day;
+    }
+
+    public void setday(String day){
+        this.day = day;
+    }
+    public String gettime(){
+        return time;
+    }
+
+    public void settime(String time){
+        this.time = time;
+    }
+    public String getdate(){
+        return date;
+    }
+
+    public void setdate(String date){
+        this.date = date;
+    }
+    public String gettimer(){
+        return timer;
+    }
+
+    public void settimer(String timer){
+        this.timer = timer;
+    }
+
+    public void setdate1(String textdate) {
+    }
+
+    public void setdateend(String dateend) {
+        this.dateend = dateend;
+    }
+
+    public String getdateend() {
+        return dateend;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getnameNoti(){
+        return nameNoti;
+    }
+
+    public void setNameNoti(String nameNoit){
+        this.name = nameNoti;
+    }
+}
+
